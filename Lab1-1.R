e6d7f5e95e9a06a97524676b97d8c0b4218d982b
ponsv690 <ponsv690@tlhw-3-5.ad.liu.se>
2019-04-04 14:50:18 +0200

    Started on report

--- Lab1-1.R
+++ Lab1-1.R
@@ -6,29 +6,38 @@ s = 14
 n = 20
 a0 = b0 = 2
 f = n - s
+a_n = a0 + s
+b_n = b0 + f
 
-rand.samples = rbeta(n*100, a0 + s, b0 + f)
+rand.samples = rbeta(n*100, a_n, b_n)
 
 # True values
-mean = (a0 + s) / (a0 + s + b0 + f)
-variance = ((a0 + s) * (b0 + f)) / (((a0 + s) + (b0 + f))^2 * ((a0 + s) + (b0 + f) + 1))
+mean = (a_n) / (a_n+b_n)
+variance = ((a_n) * (b_n)) / (((a_n) + (b_n))^2 * ((a_n) + (b_n) + 1))
 stdev = variance^0.5
 
 # Plots
-hist(rand.samples,freq=FALSE)
+hist(rand.samples,freq=FALSE,main="Histogram of beta distribution")
+abline(v=mean,col="dodgerblue4",lwd=2)
+abline(v=mean-stdev,col="darkgreen",lwd=2)
+abline(v=mean+stdev,col="darkgreen",lwd=2)
+legend("topleft", col = c("dodgerblue4", "darkgreen", "red"), 
+       legend=c("Mean","Standard deviation", "PDF of beta"),lty=1)
 x = seq(0,1,length=100)
-curve(dbeta(x,a0+s,b0+f),add=TRUE,col="red")
+curve(dbeta(x,a_n,b_n),add=TRUE,col="red",lwd=2)
 
 ### 1 b)
 set.seed(235)
 
 nDraws = 10000
-rand.samples = rbeta(nDraws, a0 + s, b0 + f)
+rand.samples = rbeta(nDraws, a_n, b_n)
 p_calc = 100*sum(rand.samples<0.4)/length(rand.samples)
 p_true = 100*pbeta(0.4, a0+s,b0+f)
 
 ### 1 c)
 
 prior = beta(a0, b0)
-hist(log(rand.samples/(1-rand.samples)), freq=FALSE)
-lines(density(log(rand.samples/(1-rand.samples))))
+hist(log(rand.samples/(1-rand.samples)), freq=FALSE, 
+     main = "Histogram of the log-odds",xlab="")
+lines(density(log(rand.samples/(1-rand.samples))),col="red",lwd=2)
+legend("topright", col = c("red"), legend=c("PDF of the log-odds"),lty=1)
--- Lab1-2.R
+++ Lab1-2.R
@@ -11,11 +11,12 @@ tau2=sum((y-3.5)^2)/10
 tau = tau2**(0.5)
 
 var<-((n-1)*tau2)/rchisq(draws,n-1)
-hist(var,breaks = seq(0,3,length=100),freq=FALSE) 
+hist(var,breaks = seq(0,3,length=100),freq=FALSE, xlab="Variance", main="Histogram of variance") 
 theta = seq(0,3,length=100)
 p = ((v/2)^(v/2))*(tau^v)*(theta^-((v/2)+1))*exp((-v*tau2)/(2*theta))
 p = p/gamma(v/2)
-lines(theta,p,col="red")
+lines(theta,p,col="red", lwd=2)
+legend("topright", col = c("red"), legend=c("PDF of the Scaled inverse chi-squared"),lty=1)
 
 ### 2 b)
 set.seed(235)
