76f69a61fb6e21d111fb05866c4a0eed7088f6aa
krisi211 <krisi211@su13-103.ad.liu.se>
2019-04-11 11:50:14 +0200

    Started on lab 1b

--- Lab2/Lab2-1.R
+++ Lab2/Lab2-1.R
@@ -1,12 +1,13 @@
 data =read.delim("/home/krisi211/Desktop/TDDE07/Lab2/TempLinkoping.txt")
 
-set.seed(123)
+set.seed(235)
 
+## 1a
 Ndraws = 10000
-mu_0 = t(data.frame(-10,100,-100))
-Omega_0 = 0.01*diag(3)
-nu_0 = 4
-var_0 = 1 
+mu_0 = t(data.frame(-15,150,-150))
+Omega_0 = 1*diag(3)
+nu_0 = 40
+var_0 = 1
 
 
 var<-(nu_0*var_0)/rchisq(Ndraws,nu_0)
@@ -14,7 +15,31 @@ hist(var,freq = FALSE)
 
 beta = matrix(0,Ndraws,3)
 library(mvtnorm)
-for(i in 1:Ndraws){
-  beta[i,] = rmvnorm(n=1,mean=mu_0,sigma=var[i]*solve(Omega_0))
+
+sim.beta = function(sigma2){
+  rmvnorm(n=1,mean=mu_0,sigma=sigma2*solve(Omega_0))
 }
 
+betas = sapply(var,sim.beta)
+betas = t(betas)
+
+prior =function(betas,time){
+  #FRÅGA!!!!!: Hur får man in epsilon i ekvationen?
+  temp = betas[1] + betas[2]*time + betas[3]*time^2
+  return(temp)
+}
+
+prior.temp = apply(betas[seq(1,25,1),],1,prior, time = data$time)
+x = dim(prior.temp)[1]
+x.axis = (1:x) / x
+#FRÅGA!!!!: Hur tätt ska kurvorna ligga varandra?
+plot(x.axis,prior.temp[,1],type = "l", xlab = "Time", ylab = "Temperature",ylim=c(-20,30))
+for (i in 2:25) {
+  lines(x.axis,prior.temp[,i])
+}
+
+## 1b
+X = cbind(rep(1,length(data$time)),data$time,data$time^2)
+beta.hat = solve(t(X)%*%X)%*%t(X)%*%data$temp
+mu_n = solve((t(X)%*%X + Omega_0))%*%(t(X)%*%X%*%beta.hat + Omega_0%*%mu_0)
+Omega_n
