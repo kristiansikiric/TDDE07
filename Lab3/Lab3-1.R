aff325845ea6afe57407a5a819bf7e5d5144109f
krisi211 <krisi211@tlhw-3-5.ad.liu.se>
2019-05-06 17:05:22 +0200

    Started on la3

---
+++ Lab3/Lab3-1.R
@@ -0,0 +1,38 @@
+data =read.delim("/home/krisi211/Desktop/TDDE07/Lab3/rainfall.dat")
+set.seed(123)
+## a)
+# Init values
+x = mean(data[,1])
+n = length(data[,1])
+
+###### Random values #######
+mu_0 = 30
+tau_0 = 10
+nu_0 = 3
+sigma_0 = 1
+sigma = 1 #Init sigma value
+############################
+
+nu_n = nu_0 + n
+NDraws = 5000
+
+#Gibbs sampling
+gibbsDraws = matrix(0,NDraws,2)
+for( i in 1:NDraws){
+  
+  #####FROM LECTURE 2#######
+  w = (n/sigma) / ((n/sigma) + (1/tau_0))
+  mu_n = w*x + (1-w)*mu_0
+  tau_n = 1/((n/sigma) + (1/tau_0))
+  ##########################
+  
+  mu = rnorm(1,mu_n,sqrt(tau_n))
+  gibbsDraws[i,1] = mu
+  
+  tau = (nu_0*sigma_0 + sum((data[,1]-mu)^2))/(n+nu_0)
+  sigma = ((nu_n-1)-tau)/rchisq(1,nu_n-1)
+  gibbsDraws[i,2] = sigma
+}
+
+hist(gibbsDraws[,1], breaks = 80)
+hist(gibbsDraws[,2], breaks = 80)
