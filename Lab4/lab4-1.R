433be5e197dd3097c6155e2b3c1c56572d21b1c3
Your Name <ponsv690@su00-107.ad.liu.se>
2019-05-20 16:22:53 +0200

    Finished Lab 4

--- Lab4/lab4-1.R
+++ Lab4/lab4-1.R
@@ -26,12 +26,12 @@ for (i in 1:length(phi)){
   ar_sims[i,] = ar.sim(phi[i])
 }
 #Hur ska man tolka det här?
-plot(ar_sims[1,],type = 'l', main = paste("phi = ",phi[1]))
-plot(ar_sims[10,],type = 'l',main = paste("phi = ",phi[11]))
-plot(ar_sims[20,],type = 'l', main = paste("phi = ",phi[21]))
+plot(ar_sims[1,],type = 'l', main = paste("phi = ",phi[1]), ylab = "xt")
+plot(ar_sims[11,],type = 'l',main = paste("phi = ",phi[11]), ylab = "xt")
+plot(ar_sims[21,],type = 'l', main = paste("phi = ",phi[21]), ylab = "xt")
 
 ## b)
-library('rstan')
+suppressMessages(library('rstan'))
 
 x = ar.sim(0.3)
 y = ar.sim(0.95)
@@ -56,11 +56,101 @@ model{
   }
 }
 '
+x.fit = stan(model_code = StanModel, data = list(N = T, x=x),refresh = 0)
+y.fit = stan(model_code = StanModel, data = list(N = T, x=y),refresh = 0)
 
-x.fit = stan(model_code = StanModel, data = list(N = T, x=x))
-print(x.fit, digits_summary = 3)
+print("Parameters for x")
+print(x.fit, digits_summary = 3,pars = c('mu','sigma2','phi'),probs = c(0.025,0.975))
+cat("\nParameters for y\n")
+print(y.fit,digits_summary = 3,pars = c('mu','sigma2','phi'),probs = c(0.025,0.975))
 
-y.fit = stan(model_code = StanModel, data = list(N = T, x=y),control = list(adapt_delta = 0.9))
-print(y.fit,digits_summary = 3)
+plot(extract(x.fit)$mu,type = 'l',ylab = c("mu"), main = paste("phi = 0.3"))
+plot(extract(y.fit)$mu, type = 'l',ylab="mu", main = paste("phi = 0.95"))
+plot(extract(x.fit)$mu,extract(x.fit)$phi)
+plot(extract(y.fit)$mu,extract(y.fit)$phi)
+
+## c)
+data = read.delim("~/Documents/TDDE07/Lab4/campy.dat")
+
+#xt = ar.sim(0.3)
+
+StanModel2 = '
+data{
+  int<lower=1> N;
+  int c[N];
+}
+parameters{
+  real x[N];
+  real mu;
+  real phi;
+  real sigma2;
+}
+transformed parameters {
+  real sigma;
+  sigma = sqrt(sigma2);
+}
+model{
+  phi ~ uniform(-1, 1);
+  for(n in 2:N){
+    x[n] ~ normal(mu + phi*(x[n-1]-mu), sigma);
+    c[n-1] ~ poisson(exp(x[n-1]));
+  }
+  c[N] ~ poisson(exp(x[N]));
+}
+'
+
+fit.stan = function(stanModel, data){
+  c.fit = stan(model_code = stanModel,data = list(N = length(data$c),c=data$c))
+}
+
+plot.stan = function(model, data){
+  #Last five elements were not wanted.
+  mean = exp(summary(model)$summary[1:140,'mean'])
+  cred_97.5 = exp(summary(model)$summary[1:140,'97.5%'])
+  cred_2.5 = exp(summary(model)$summary[1:140,'2.5%'])
+  plot(data$c)
+  lines(mean,col="green")
+  lines(cred_2.5,col="red")
+  lines(cred_97.5,col="blue")
+  legend("topleft", legend = c("Mean", "2.5% cred. interval", "97.5% cred. interval"), col = c("green", "red", "blue"), lty = 1, cex = 0.8)
+}
+
+c.fit = fit.stan(StanModel2, data)
+plot.stan(c.fit, data)
+
+## d)
+
+StanModel3 = '
+data{
+  int<lower=1> N;
+  int c[N];
+}
+parameters{
+  real x[N];
+  real mu;
+  real phi;
+  real<lower=0> sigma2;
+}
+transformed parameters {
+  real sigma;
+  sigma = sqrt(sigma2);
+}
+model{
+  phi ~ uniform(-1, 1);
+  sigma2 ~ scaled_inv_chi_square(N,0.05);
+  for(n in 2:N){
+    x[n] ~ normal(mu + phi*(x[n-1]-mu), sigma);
+    c[n-1] ~ poisson(exp(x[n-1]));
+  }
+  c[N] ~ poisson(exp(x[N]));
+}
+'
+
+c.fit = fit.stan(StanModel3, data)
+plot.stan(c.fit, data)
+
+
+
+
 
 
